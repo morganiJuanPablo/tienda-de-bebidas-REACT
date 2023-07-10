3792ac16699550c9e0bae26a60a8094c0c9720c2
Juan Pablo Morgani <morgani.jpg@gmail.com>
2023-07-10 15:53:53 +0200

    subiendo lo ultimo de la clase 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+<<<<<<< Updated upstream
+=======
+import { BrowserRouter, Routes, Route } from "react-router-dom"; ///Indican que a partir de la importación nuestro proyecto va a tener un sistema de rutas. Tienen etiqueta de cierre.
+>>>>>>> Stashed changes
 import NavbarContainer from "./compoments/layouts/navbar/NavbarContainer";
 import HomeContainer from "./compoments/pages/home/HomeContainer";
 import { useState } from "react";
 
 function App() {
+<<<<<<< Updated upstream
   const [contador, setContador] = useState(0);
   const sumar = () => {
     setContador(contador + 1);
@@ -16,3 +21,35 @@ function App() {
 }
 
 export default App
+=======
+  return (
+    ///Esta es la estructura que siempre va a quedar así. Es el sistema de rutas que vamos a usar siempre.
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<NavbarContainer />}>
+          {" "}
+          {/* Al ser una ruta padre se reciben los hijos como children pero en realidad con React Routes son Outlet*/}
+          <Route path="/" element={<HomeContainer />} />
+          <Route path="/category/:categoryName" />
+          <Route
+            path="/cart"
+            /* Asi le decimos que estamos iniciando la ruta raíz */ element={
+              <h1>AOOOOLO</h1>
+            } /* En element pueden ir etiquetas o bien ya los componentes armados */
+          />
+          <Route
+            path="/premiados"
+            /* Asi le decimos que estamos iniciando la ruta raíz */ element={
+              <PremiadosContainer />
+            } /* En element pueden ir etiquetas o bien ya los componentes armados */
+          />
+        </Route>
+
+        <Route path="*" element={<h1>NOT FOUND</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+>>>>>>> Stashed changes
