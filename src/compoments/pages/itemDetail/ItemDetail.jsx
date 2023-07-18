04dfc0922286254ba7e6792c6fd82b3bf32c24c7
Juan Pablo Morgani <morgani.jpg@gmail.com>
2023-07-18 21:40:22 +0200

    #

--- src/compoments/pages/itemDetail/ItemDetail.jsx
+++ src/compoments/pages/itemDetail/ItemDetail.jsx
@@ -3,36 +3,37 @@ import styles from "./ItemDetail.module.css";
 
 const ItemDetail = ({ product, agregarCarrito }) => {
   return (
-    <div className={styles.contenedorPadre}>
-      <div className={styles.contenedor}>
-        <div className={styles.contenedorImg}>
-          <img src={product.img} />
-        </div>
-        <div className={styles.contenedorInfo}>
-          <div>
-            <h2 className={styles.nombreProducto}>{product.tittle}</h2>
-            <p className={styles.detalleProducto}>{product.description}</p>
+    <>
+      <div className={styles.contenedorPadre}>
+        <div className={styles.contenedor}>
+          <div className={styles.contenedorImg}>
+            <img src={product.img} />
           </div>
-          <div className={styles.precioProducto}>
-            <p>
-              <b>{product.price}</b>€
-            </p>
-          </div>
-          <div className={styles.logicaCarrito}>
-            <Contador />
-            <button className={styles.btnAñadir}>Añadir</button>
-            <button className={styles.btnIrCarrito}>Ir al carrito</button>
-          </div>
-          <div className={styles.contenedorIconos}>
+          <div className={styles.contenedorInfo}>
             <div>
-              <img
-                className={styles.icono}
-                src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689250699/ProyectoReactCoder/IconsIremDetail/Icons-02_migynq.png"
-                alt="Presentación"
-              />
+              <h2 className={styles.nombreProducto}>{product.tittle}</h2>
+              <p className={styles.detalleProducto}>{product.description}</p>
             </div>
+            <div className={styles.precioProducto}>
+              <p>
+                <b>{product.price}</b>€
+              </p>
+            </div>
+            <div className={styles.logicaCarrito}>
+              <Contador />
+              <button className={styles.btnAñadir}>Añadir</button>
+              <button className={styles.btnIrCarrito}>Ir al carrito</button>
+            </div>
+            <div className={styles.contenedorIconos}>
+              <div>
+                <img
+                  className={styles.icono}
+                  src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689250699/ProyectoReactCoder/IconsIremDetail/Icons-02_migynq.png"
+                  alt="Presentación"
+                />
+              </div>
 
-            <p className={styles.detallesIcono}>hfghfghfgh</p>
+              <p className={styles.detallesIcono}>hfghfghfgh</p>
 
               <div>
                 <img
@@ -42,7 +43,7 @@ const ItemDetail = ({ product, agregarCarrito }) => {
                 />
               </div>
 
-            <p className={styles.detallesIcono}>sfsdfsdfsdf</p>
+              <p className={styles.detallesIcono}>sfsdfsdfsdf</p>
 
               <div>
                 <img
@@ -52,31 +53,33 @@ const ItemDetail = ({ product, agregarCarrito }) => {
                 />
               </div>
 
-            <p className={styles.detallesIcono}>sdfsdfsdf</p>
+              <p className={styles.detallesIcono}>sdfsdfsdf</p>
 
-            <div>
-              <img
-                className={styles.icono}
-                src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689250886/ProyectoReactCoder/IconsIremDetail/Icons-01_uuphhk.png"
-                alt="Graduación"
-              />
+              <div>
+                <img
+                  className={styles.icono}
+                  src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689250886/ProyectoReactCoder/IconsIremDetail/Icons-01_uuphhk.png"
+                  alt="Graduación"
+                />
+              </div>
+              <p className={styles.detallesIcono}>sdasdasd</p>
             </div>
-            <p className={styles.detallesIcono}>sdasdasd</p>
+          </div>
+        
+        <div className={styles.contenedorBannerEnvios}>
+          <div className={styles.contenidoBannerEnvios}>
+            <img src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689623981/ProyectoReactCoder/Footer/truckBannerEnvios-01_oarao9.png" />
+            <p className={styles.envioGratisTittle}>
+              <b>envío</b>gratis
+            </p>
+            <p className={styles.envioGratisDescription}>
+              Si vives en Madrid. Revisa los términos y condiciones. Comienza a
+              disfrutar de este beneficio desde hoy hasta lo que quede de este
+              2023.
+            </p>
           </div>
         </div>
       </div>
-      <div className={styles.contenedorBannerEnvios}>
-        <div className={styles.contenidoBannerEnvios}>
-          <img src="https://res.cloudinary.com/dqykftyy6/image/upload/v1689623981/ProyectoReactCoder/Footer/truckBannerEnvios-01_oarao9.png" />
-          <p className={styles.envioGratisTittle}>
-            <b>envío</b>gratis
-          </p>
-          <p className={styles.envioGratisDescription}>
-            Si vives en Madrid. Revisa los términos y condiciones. Comienza a
-            disfrutar de este beneficio desde hoy hasta lo que quede de este
-            2023.
-          </p>
-        </div>
       </div>
     </>
   );
